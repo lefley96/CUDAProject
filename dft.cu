4bff4d3a41895c35406700fe41c7bd06c75fa424
kazet15 <kazet15@gmail.com>
2015-06-04 21:16:58 +0200

    fix indentation

--- dft.cu
+++ dft.cu
@@ -4,87 +4,89 @@
 __constant__ float EXPCONST = (-2.0 * 3.141592653);
 
 __device__ __forceinline__ cuComplex my_cexpf(cuComplex z) {
-	cuComplex res;
-	float t = expf(z.x);
+    cuComplex res;
+    float t = expf(z.x);
 
-	sincosf(z.y, &res.y, &res.x);
-	res.x *= t;
-	res.y *= t;
+    sincosf(z.y, &res.y, &res.x);
+    res.x *= t;
+    res.y *= t;
 
-	return res;
+    return res;
 }
  
 __global__ void dftKernel(cuComplex *input, cuComplex *output, int size) {
-	int idx = blockDim.x * blockIdx.x + threadIdx.x;
+    int idx = blockDim.x * blockIdx.x + threadIdx.x;
 
-	if (idx < size) {
-		cuComplex tempSum = make_cuComplex(0.0, 0.0);
+    if (idx < size) {
+        cuComplex tempSum = make_cuComplex(0.0, 0.0);
 
-		for (int i = 0; i < size; i++) {
-			tempSum = cuCaddf(tempSum, cuCmulf(input[i], my_cexpf(make_cuComplex(0.0, (EXPCONST * i * idx / size)))));
-		}
+        for (int i = 0; i < size; i++) {
+            tempSum = cuCaddf(tempSum, cuCmulf(input[i], my_cexpf(make_cuComplex(0.0, (EXPCONST * i * idx / size)))));
+        }
 
-		output[idx] = tempSum;
-	}
+        output[idx] = tempSum;
+    }
 }
 
 int chooseAndSetBestDevice() {
-	int num_devices, device;
-	int max_multiprocessors = 0, max_device = 0;
+    int num_devices, device;
+    int max_multiprocessors = 0, max_device = 0;
+    
+    cudaGetDeviceCount(&num_devices);
 	
-	cudaGetDeviceCount(&num_devices);
-	if (num_devices > 1) {
-		  
-		  for (device = 0; device < num_devices; device++) {
-				  cudaDeviceProp properties;
-				  cudaGetDeviceProperties(&properties, device);
-				  if (max_multiprocessors < properties.multiProcessorCount) {
-						  max_multiprocessors = properties.multiProcessorCount;
-						  max_device = device;
-				  }
+    if (num_devices > 1) {          
+		for (device = 0; device < num_devices; device++) {
+			cudaDeviceProp properties;
+			cudaGetDeviceProperties(&properties, device);
+			
+			if (max_multiprocessors < properties.multiProcessorCount) {
+				max_multiprocessors = properties.multiProcessorCount;
+				max_device = device;
+			}
 		}
-	}
-	cudaSetDevice(max_device);
-	return max_device; 
+    }
+	
+    cudaSetDevice(max_device);
+    return max_device; 
 }
 
 int getThreadsPerBlock(int currentDevice) {
-	cudaDeviceProp properties;
-	cudaGetDeviceProperties(&properties, currentDevice);
-	return properties.maxThreadsPerBlock;
+    cudaDeviceProp properties;
+    cudaGetDeviceProperties(&properties, currentDevice);
+    return properties.maxThreadsPerBlock;
 }
  
 extern "C" void dft(float* samples, int size, float* real, float* imag) {
-	int currentDevice = chooseAndSetBestDevice();
-	int THREADS_PER_BLOCK = getThreadsPerBlock(currentDevice);
+    int currentDevice = chooseAndSetBestDevice();
+    int THREADS_PER_BLOCK = getThreadsPerBlock(currentDevice);
 
-	cuComplex* complex_samples = (cuComplex*) malloc(size * sizeof(cuComplex));
+    cuComplex* complex_samples = (cuComplex*) malloc(size * sizeof(cuComplex));
 
-	int i;
-	for (i = 0; i < size; i++) {
+    int i;
+    for (i = 0; i < size; i++) {
 		complex_samples[i] = make_cuComplex(samples[i], 0);
-	}
+    }
 
-	cuComplex *d_input, *d_output;
+    cuComplex *d_input, *d_output;
  
-	cudaMalloc((void **) &d_input, size * sizeof(cuComplex));
-	cudaMalloc((void **) &d_output, size * sizeof(cuComplex));
+    cudaMalloc((void **) &d_input, size * sizeof(cuComplex));
+    cudaMalloc((void **) &d_output, size * sizeof(cuComplex));
 
-	cudaMemcpy(d_input, complex_samples, size * sizeof(cuComplex), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_input, complex_samples, size * sizeof(cuComplex), cudaMemcpyHostToDevice);
 
-	dftKernel<<<(size + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_input, d_output, size);
+    dftKernel<<<(size + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_input, d_output, size);
 
-	cudaDeviceSynchronize();
+    cudaDeviceSynchronize();
 
-	cudaMemcpy(complex_samples, d_output, size * sizeof(cuComplex), cudaMemcpyDeviceToHost);
+    cudaMemcpy(complex_samples, d_output, size * sizeof(cuComplex), cudaMemcpyDeviceToHost);
 
-	for (i = 0; i < size; i++) {
-		real[i] = cuCrealf(complex_samples[i]);
-		imag[i] = cuCimagf(complex_samples[i]);
-	}
+    for (i = 0; i < size; i++) {
+        real[i] = cuCrealf(complex_samples[i]);
+        imag[i] = cuCimagf(complex_samples[i]);
+    }
 
-	cudaFree(d_output);
-	cudaFree(d_input);
-	free(complex_samples);
+    cudaFree(d_output);
+    cudaFree(d_input);
+    free(complex_samples);
 }
 
--- main.c
+++ main.c
@@ -10,128 +10,131 @@
 void dft(float* samples, int size, float* real, float* imag);
 
 int main(int argc, char **argv) {
-	int myRank, procCount;
-        MPI_Status status;
-        MPI_Init(&argc, &argv);
-        MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-        MPI_Comm_size(MPI_COMM_WORLD, &procCount);
+    int myRank, procCount;
+	MPI_Status status;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+	MPI_Comm_size(MPI_COMM_WORLD, &procCount);
 
-	int i, j;
+    int i, j;
 
-	int samples;
-	int channels;
+    int samples;
+    int channels;
 
-	float** output_real;
-	float** output_imag;
+    float** output_real;
+    float** output_imag;
 
-	// master reads data from the input file and splits data among workers
-        if (myRank == 0) {
-                if (argc < 3) {
-                        printf("please specify number of samples and channels as arguments\n");
-                        return 0;
-                }
+    // master reads data from the input file and splits data among workers
+	if (myRank == 0) {
+		if (argc < 3) {
+			printf("please specify number of samples and channels as arguments\n");
+			return 0;
+		}
 
-                samples = atoi(argv[1]);
-                channels = atoi(argv[2]);
+		samples = atoi(argv[1]);
+		channels = atoi(argv[2]);
 
-                printf("Reading file with %d channels of %d samples each\n", channels, samples);
+		printf("Reading file with %d channels of %d samples each\n", channels, samples);
 
-                float** input = (float**) malloc(channels * sizeof(float*));
+		float** input = (float**) malloc(channels * sizeof(float*));
 
-		for (i = 0; i < channels; i++) {
+        for (i = 0; i < channels; i++) {
 			input[i] = (float*) malloc(samples * sizeof(float));
+        }
+
+		FILE* stream = fopen("input.txt", "r");
+
+		// load data from input file
+		for (i = 0; i < samples; i++) {
+			for (j = 0; j < channels; j++) {
+				int dummy = fscanf(stream, "%f", &input[j][i]);
+			}
 		}
 
-                FILE* stream = fopen("input.txt", "r");
+        fclose(stream);
 
-                // load data from input file
-                for (i = 0; i < samples; i++) {
-                        for (j = 0; j < channels; j++) {
-                                int dummy = fscanf(stream, "%f", &input[j][i]);
-                        }
-                }
-
-		fclose(stream);
-
-		// print channels and samples
-                for (i = 0; i < channels; i++) {
-                        printf("Channel %d: ", i + 1);
-                        for (j = 0; j < samples; j++) {
-				printf("%.0f", input[i][j]);
-								
-                                if (j != samples - 1) {
-                                        printf(", ");
-                                }
-                                else {
-                                        printf("\n");
-                                }
-                        }
-                }
-
-		printf("\n");
-		
-                // send data to workers
-                for (i = 0; i < channels; i++) {
-			MPI_Send(&samples, 1, MPI_INT, i + 1, DATA, MPI_COMM_WORLD);
-                        MPI_Send(input[i], samples, MPI_FLOAT, i + 1, DATA, MPI_COMM_WORLD);
-                }
-
-                // allocate memory for computation results
-		output_real = (float**) malloc(channels * sizeof(float*));
-		output_imag = (float**) malloc(channels * sizeof(float*));
+        // print channels and samples
 		for (i = 0; i < channels; i++) {
-			output_real[i] = (float*) malloc(samples * sizeof(float));
-			output_imag[i] = (float*) malloc(samples * sizeof(float));
+			printf("Channel %d: ", i + 1);
+			
+			for (j = 0; j < samples; j++) {
+				printf("%.0f", input[i][j]);
+						
+				if (j != samples - 1) {
+						printf(", ");
+				}
+				else {
+					printf("\n");
+				}
+			}
 		}
 
-		// gather results from workers
-                for (i = 0; i < channels; i++) {
+        printf("\n");
+        
+		// send data to workers
+		for (i = 0; i < channels; i++) {
+			MPI_Send(&samples, 1, MPI_INT, i + 1, DATA, MPI_COMM_WORLD);
+			MPI_Send(input[i], samples, MPI_FLOAT, i + 1, DATA, MPI_COMM_WORLD);
+		}
+
+		// allocate memory for computation results
+        output_real = (float**) malloc(channels * sizeof(float*));
+        output_imag = (float**) malloc(channels * sizeof(float*));
+        for (i = 0; i < channels; i++) {
+            output_real[i] = (float*) malloc(samples * sizeof(float));
+            output_imag[i] = (float*) malloc(samples * sizeof(float));
+        }
+
+        // gather results from workers
+		for (i = 0; i < channels; i++) {
 			MPI_Recv(output_real[i], samples, MPI_FLOAT, i + 1, DATA, MPI_COMM_WORLD, &status);
 			MPI_Recv(output_imag[i], samples, MPI_FLOAT, i + 1, DATA, MPI_COMM_WORLD, &status);
-                }
-        }
-	// worker computes data from each channel
-	else {
-		int received_samples;
-		MPI_Recv(&received_samples, 1, MPI_INT, MPI_ANY_SOURCE, DATA, MPI_COMM_WORLD, &status);
+		}
+	}
+	
+    // worker computes data from each channel
+    else {
+        int received_samples;
+        MPI_Recv(&received_samples, 1, MPI_INT, MPI_ANY_SOURCE, DATA, MPI_COMM_WORLD, &status);
 
-		float* received_input = (float*) malloc(received_samples * sizeof(float));
-		MPI_Recv(received_input, received_samples, MPI_FLOAT, MPI_ANY_SOURCE, DATA, MPI_COMM_WORLD, &status);
+        float* received_input = (float*) malloc(received_samples * sizeof(float));
+        MPI_Recv(received_input, received_samples, MPI_FLOAT, MPI_ANY_SOURCE, DATA, MPI_COMM_WORLD, &status);
 
-                // gpu kernel invocation
-		float* output_real = (float*) malloc(received_samples * sizeof(float));
-		float* output_imag = (float*) malloc(received_samples * sizeof(float));
-		dft(received_input, received_samples, output_real, output_imag);
+		// gpu kernel invocation
+        float* output_real = (float*) malloc(received_samples * sizeof(float));
+        float* output_imag = (float*) malloc(received_samples * sizeof(float));
+        dft(received_input, received_samples, output_real, output_imag);
 
-		MPI_Send(output_real, received_samples, MPI_FLOAT, 0, DATA, MPI_COMM_WORLD);
-		MPI_Send(output_imag, received_samples, MPI_FLOAT, 0, DATA, MPI_COMM_WORLD);
-		
-		free(received_input);
-		free(output_real);
-		free(output_imag);
-        }
-
-	MPI_Barrier(MPI_COMM_WORLD);
-
-	if (myRank == 0) {
-        	printf("Received results from all workers\n");
-
-                // print out results
-		for (i = 0; i < channels; i++) {
-                        printf("Channel %d: ", i + 1);
-                        for (j = 0; j < samples; j++) {
-                                printf("%.2f %.2fi", output_real[i][j], output_imag[i][j]);
-
-                                if (j != samples - 1) {
-                                        printf(", ");
-                                }
-                                else {
-                                        printf("\n");
-                                }
-                        }
-                }
+        MPI_Send(output_real, received_samples, MPI_FLOAT, 0, DATA, MPI_COMM_WORLD);
+        MPI_Send(output_imag, received_samples, MPI_FLOAT, 0, DATA, MPI_COMM_WORLD);
+        
+        free(received_input);
+        free(output_real);
+        free(output_imag);
 	}
 
-        MPI_Finalize();
-        return 0;
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (myRank == 0) {
+		printf("Received results from all workers\n");
+
+		// print out results
+        for (i = 0; i < channels; i++) {
+			printf("Channel %d: ", i + 1);
+			
+			for (j = 0; j < samples; j++) {
+				printf("%.2f %.2fi", output_real[i][j], output_imag[i][j]);
+
+				if (j != samples - 1) {
+					printf(", ");
+				}
+				else {
+					printf("\n");
+				}
+			}
+		}
+    }
+
+	MPI_Finalize();
+	return 0;
 }
